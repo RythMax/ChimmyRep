eddac3eac4a61813b02f10598e58c7705080610f
ABRAHAN ESTEBAN PACHANO JIMENEZ <apachano20190261@ucsd.edu.do>
2022-07-14 16:06:47 -0400

    It took me a While but I made these views on the page, and I can Make it login, with just an update database you can get the database, although.... You must change some code to get the cycle going

---
+++ TomyChimmy/TomyChimmy/ViewModels/UserRolesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TomyChimmy.ViewModels
+{
+    public class UserRolesViewModel
+    {
+        public string Id { get; set; }
+
+        public string Nombres { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
